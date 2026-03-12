f353c44fef9fe553c6950c47f78428b904519189
Carlos Alvarez <23004004@galileo.edu>
2026-03-12 08:02:18 -0600

    Logica para calacular cuantos bytes va a leer el  thread (Solo uno por el momento)

--- log_processor.h
+++ log_processor.h
@@ -4,13 +4,19 @@
 #include <pthread.h>
 
 #define HASH_SIZE 10007
-#define NUM_THREADS   4
+#define NUM_THREADS   1
 
 typedef struct Entry {
     char key[256];
     int count;
     struct Entry *next;
 } Entry;
+
+typedef struct {
+    const char *filename;
+    long start_byte;
+    long end_byte;
+} ThreadArgs;
 
 long get_file_size(const char *filename);
 void proccesFile(const char *filename);
--- main_linux.c
+++ main_linux.c
@@ -9,6 +9,8 @@ void* createThread(void *arg);
 int main(){
     pthread_t hilo;
     const char *filename = "access.log";
+    long start_byte = 0;
+    long end_byte = 0;
 
     long file_size = get_file_size(filename);
     if (file_size <= 0) {
@@ -16,7 +18,14 @@ int main(){
         return 1;
     }
 
-    pthread_create(&hilo, NULL, createThread, (void*)filename);
+    end_byte = file_size / NUM_THREADS;
+    ThreadArgs *new_thread = malloc(sizeof(ThreadArgs));
+
+    new_thread -> filename = filename;
+    new_thread -> start_byte = start_byte;
+    new_thread -> end_byte = end_byte;
+
+    pthread_create(&hilo, NULL, createThread, (void*)new_thread);
 
 
     pthread_join(hilo, NULL);
@@ -24,7 +33,10 @@ int main(){
 }
 
 void* createThread(void *arg){
-    const char *filename = (const char*)arg;
-    proccesFile(filename);
+    ThreadArgs *new_thread = (ThreadArgs*)arg;
+
+    printf("Leyendo desde byte %ld hasta byte %ld\n", new_thread->start_byte, new_thread->end_byte);
+
+    //proccesFile(new_thread -> filename);
     return NULL;
 }
